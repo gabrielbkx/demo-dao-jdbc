cf5bad5c84c51e7ca5be16feb8e9d42ec10ee6d8
Gabriel <oliveirafrerreira97@hotmail.com>
2024-11-24 23:36:21 -0300

    department-entities-class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,13 @@
+package model.entities;
+
+import java.sql.Date;
+
+public class Seller {
+    private int id;
+    private String name;
+    private String email;
+    private Date birthday;
+    private Double baseSalary;
+
+    Department department;
+}
